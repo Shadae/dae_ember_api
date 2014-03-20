2ba137605a6f1c388ca9dcd939df11b7710b3570
Shadae Holmes <ShadaeHolmes@gmail.com>
2014-03-20 11:21:07 -0700

    Product changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,14 +8,22 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.new(params.require(:product).permit(:name, :price, :description, :image, :avatar))
+    @product = Product.new(params.require(:product).permit(:name,
+                                                           :price,
+                                                           :description,
+                                                           :image,
+                                                           :avatar))
     @product.save
     render "show"
   end
 
   def update
     @product = Product.find(params[:id])
-    @product.update(params.require(:product).permit(:name, :price, :description, :image, :avatar))
+    @product.update(params.require(:product).permit(:name,
+                                                    :price,
+                                                    :description,
+                                                    :image,
+                                                    :avatar))
     render "show"
   end
 
